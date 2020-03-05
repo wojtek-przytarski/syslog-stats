775c7aa0ad00bd866aaf877f5cbbe36a8181e879
Wojtek Przytarski <vojtek16@gmail.com>
2020-03-05 01:10:26 +0100

    Reuse processes and add Statistics class

--- reader.py
+++ reader.py
@@ -5,11 +5,12 @@ import logging
 from collections import defaultdict
 from multiprocessing import Queue, Pool, cpu_count
 
+from statistics import StatisticsManager
 
 RFC3164_PATTERN = re.compile(r'<(\d{1,3})>(.{15})\s(\S*)\s(.*)')
 RFC3164_MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
-CHUNKING_PROCESSES = cpu_count() - 1
+CHUNKING_PROCESSES = max(1, cpu_count() - 1)
 
 
 def read_file(filename, chunk_size=1000000):
@@ -20,11 +21,9 @@ def read_file(filename, chunk_size=1000000):
 
     with open(filename) as file:
         while chunk := file.readlines(chunk_size):
-            print('.', end='')
             chunks_queue.put(chunk)
     print('Sending STOP messages')
     for i in range(CHUNKING_PROCESSES):
-        print(f'STOP #{i}')
         chunks_queue.put('STOP_CHUNKS')
     pool.close()
     pool.join()
@@ -33,51 +32,19 @@ def read_file(filename, chunk_size=1000000):
 def handle_chunk():
     stats_queue.put('START_CHUNK_PROC')
     while (chunk := chunks_queue.get()) != 'STOP_CHUNKS':
-        stats = {
-            'severe': 0,
-            'msg_length': 0,
-            'lines': 0,
-            'oldest': 'Jan  1 00:00:00',
-            'latest': 'Dec 31 23:59:59',
-            'by_host': {}
-        }
+        stats = StatisticsManager()
         for line in chunk:
-            stats['lines'] += 1
-
             line_stats = handle_line(line)
-            msg_length = line_stats['length']
-            severe = line_stats['severe']
-            hostname = line_stats['hostname']
-            timestamp = line_stats['timestamp']
-
-            stats['msg_length'] += msg_length
-            stats['severe'] += severe
-            if stats['by_host'].get(hostname):
-                stats['by_host'][hostname]['msg_length'] += msg_length
-                stats['by_host'][hostname]['severe'] += severe
-            else:
-                stats['by_host'][hostname] = {
-                    'msg_length': msg_length,
-                    'severe': severe,
-                }
-        stats_queue.put(stats)
+            stats.add_data(
+                *line_stats
+            )
+        stats_queue.put(stats.to_dict())
+        # print('stats in queue')
     stats_queue.put('END_CHUNK_PROC')
 
 
 def prepare_statistics():
-    stats = {
-        'lines': 0,
-        'severe': 0,
-        'msg_length': 0,
-        # 'oldest': 'Jan  1 00:00:00',
-        # 'latest': 'Dec 31 23:59:59',
-        'by_host': defaultdict(lambda: {
-            'severe': 0,
-            'msg_length': 0,
-            'oldest': 'Jan  1 00:00:00',
-            'latest': 'Dec 31 23:59:59',
-        })
-    }
+    stats = StatisticsManager()
     print('Starting stats')
     x = 1
     stats_queue.get()
@@ -88,28 +55,23 @@ def prepare_statistics():
         elif partial_stats == 'END_CHUNK_PROC':
             x -= 1
         else:
-            stats['lines'] += partial_stats['lines']
-            stats['msg_length'] += partial_stats['msg_length']
-            stats['severe'] += partial_stats['severe']
+            stats.add_total_data(partial_stats['total'])
             for host, host_dict in partial_stats['by_host'].items():
-                stats['by_host'][host]['severe'] += host_dict['severe']
-                stats['by_host'][host]['msg_length'] += host_dict['msg_length']
-    with open('stats.txt', 'w') as file:
-        file.writelines([
-            f'Total ALERT and EMERGENCY lines: {stats["severe"]}\n',
-            f'Average msg length: {stats["msg_length"]/stats["lines"]}\n',
-        ])
+                stats.add_data(host, host_dict)
+    stats.write_to_file('stats.csv')
+    print('Stats saved')
 
 
 def handle_line(line):
     log_data = parse_line(line)
     stats = {
-        'length': len(log_data.get('msg')),
-        'hostname': log_data.get('hostname'),
-        'timestamp': len(log_data.get('timestamp')),
-        'severe': 1 if get_severity(log_data.get('pri')) <= 1 else 0,
+        'lines': 1,
+        'messages_length': len(log_data.get('msg')),
+        'severe_messages': 1 if get_severity(log_data.get('pri')) <= 1 else 0,
+        'oldest_timestamp': log_data.get('timestamp'),
+        'latest_timestamp': log_data.get('timestamp'),
     }
-    return stats
+    return log_data.get('hostname'), stats
 
 
 def get_severity(pri):
@@ -148,15 +110,16 @@ def parse_line(line):
 if __name__ == '__main__':
     start = time.perf_counter()
 
-    file_name = 'syslog1GB'
+    file_name = 'tmp'
     file_size = os.path.getsize(file_name)
-    chunks_number = 100
+    cs = 1048000
+    chunks_number = round(file_size / cs)
 
     stats_queue = Queue(chunks_number + 20)
     chunks_queue = Queue(chunks_number + 20)
 
     print('Starting... ')
-    read_file(file_name, round(file_size / chunks_number))
+    read_file(file_name, cs)
 
     finish = time.perf_counter()
 
---
+++ statistics.py
@@ -0,0 +1,86 @@
+import csv
+import datetime
+from collections import defaultdict
+
+
+class Statistics:
+    BSD_TIMESTAMP_FORMAT = '%b %d %H:%M:%S'
+
+    def __init__(self):
+        self.lines = 0
+        self.messages_length = 0
+        self.severe_messages = 0
+        self.oldest_timestamp = self._bsd_timestamp_to_datetime('Dec 31 23:59:59')
+        self.latest_timestamp = self._bsd_timestamp_to_datetime('Jan  1 00:00:00')
+
+    def add_data(self, stats, line=True):
+        self.lines += 1 if line else stats['lines']
+        self.messages_length += stats['messages_length']
+        self.severe_messages += stats['severe_messages']
+
+        oldest = self._bsd_timestamp_to_datetime(stats['oldest_timestamp'])
+        latest = oldest if line else self._bsd_timestamp_to_datetime(stats['latest_timestamp'])
+        if oldest < self.oldest_timestamp:
+            self.oldest_timestamp = oldest
+        if latest > self.latest_timestamp:
+            self.latest_timestamp = latest
+
+    @property
+    def average_message_length(self):
+        return self.messages_length / self.lines
+
+    def to_csv_line(self, title):
+        return [
+            title,
+            self.severe_messages,
+            self.average_message_length,
+            self.oldest_timestamp,
+            self.latest_timestamp,
+        ]
+
+    def to_dict(self):
+        return {
+            'lines': self.lines,
+            'messages_length': self.messages_length,
+            'severe_messages': self.severe_messages,
+            'oldest_timestamp': self._datetime_to_bsd_timestamp(self.oldest_timestamp),
+            'latest_timestamp': self._datetime_to_bsd_timestamp(self.latest_timestamp),
+        }
+
+    def _bsd_timestamp_to_datetime(self, timestamp):
+        return datetime.datetime.strptime(timestamp, self.BSD_TIMESTAMP_FORMAT)
+
+    def _datetime_to_bsd_timestamp(self, timestamp):
+        return datetime.datetime.strftime(timestamp, self.BSD_TIMESTAMP_FORMAT)
+
+
+class StatisticsManager:
+    def __init__(self):
+        self.total = Statistics()
+        self.by_host = defaultdict(lambda: Statistics())
+
+    def add_data(self, hostname, stats, update_total=False):
+        self.by_host[hostname].add_data(stats)
+        if update_total:
+            self.add_total_data(stats)
+
+    def add_total_data(self, stats):
+        self.total.add_data(stats)
+
+    def to_dict(self):
+        return {
+            'total': self.total.to_dict(),
+            'by_host': {
+                hostname: st.to_dict()
+                for hostname, st in self.by_host.items()
+            }
+        }
+
+    def write_to_file(self, filename):
+        with open(filename, 'w') as file:
+            w = csv.writer(file)
+            w.writerow(['Host', 'ALERT/EMERGENCY messages', 'AVG message length', 'Oldest', 'Newest'])
+            w.writerow(self.total.to_csv_line('Total'))
+            w.writerow([])
+            for host, stats in self.by_host.items():
+                w.writerow(stats.to_csv_line(host))
